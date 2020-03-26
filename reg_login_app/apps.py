d5f5e9ba8c95a19a9535deed19b63b2b89707c5b
Vincent-Salazar-Dev <vincesalazar77@gmail.com>
2020-03-26 01:51:13 -0700

    intial commit

---
+++ reg_login_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class RegLoginAppConfig(AppConfig):
+    name = 'reg_login_app'
---
+++ reg_login_app/views.py
@@ -0,0 +1,64 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+import bcrypt
+from .models import User
+
+def index(request):
+    return render(request, 'index.html')
+
+def register(request):
+    post = request.POST
+    errors = User.objects.basic_validator(post)
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect('/')
+    lowerCaseEmail = post['email'].lower()
+    if User.objects.filter(email = lowerCaseEmail).exists():
+        messages.error(request, "That email already exists")
+        return redirect('/')
+    capitalizedFirstName = post['first_name'].capitalize()
+    capitalizedLastName = post['last_name'].capitalize()
+    password = post['password']
+    pw_hash = bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()
+    user = User.objects.create(
+        first_name = capitalizedFirstName, 
+        last_name = capitalizedLastName, 
+        email = lowerCaseEmail, 
+        password = pw_hash
+    )
+    request.session['user_id'] = user.id
+
+    return redirect('/success')
+
+def login(request):
+    post = request.POST
+    lowerEmail = post['email'].lower()
+    try:
+        user = User.objects.get(email = lowerEmail)
+    except:
+        messages.error(request, "Please check your password or email.")
+        return redirect('/')
+
+    if bcrypt.checkpw(request.POST['password'].encode(), user.password.encode()):
+        request.session["user_id"] = user.id
+        return redirect('/success')
+    else:
+        messages.error(request, "please check your password or email.")
+        return redirect('/')
+    
+def success(request):
+    if "user_id" not in request.session:
+        messages.error(request, "Must be logged in")
+        return redirect('/')
+    
+    user_id = request.session['user_id']
+
+    context = {
+        "user": User.objects.get(id=user_id)
+    }
+    return render(request, "success.html", context)
+
+def logout(request):
+    request.session.clear()
+    return redirect('/')
